d61de2915e9b4732e845a24233323cdb8d66d697
Karan Lingayat <karan942006@gmail.com>
2026-05-29 20:45:02 +0530

    Update ElseIf1.java

--- ElseIf1.java
+++ ElseIf1.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class ElseIf1{
 public static void main(String args[]){
 Scanner sc = new Scanner(System.in);
-System.out.println("Enter ");
+System.out.println("Enter 2 numbers.");
 int a = sc.nextInt();
 int b = sc.nextInt();
 if(a == b){
