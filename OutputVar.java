0f0adcd9ea8bb072c1906b4f7b245724b299925a
Karan Lingayat <karan942006@gmail.com>
2026-05-29 20:09:59 +0530

    Added Some Java Codes

---
+++ OutputVar.java
@@ -0,0 +1,14 @@
+public class OutputVar {
+    public static void main(String[] args) {
+        String name = "Karan";
+        double pi = 3.14;
+        boolean isStudent = false;
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Name: " + name);
+        System.out.println("Pi: " + pi);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("The sum of a and b is: " + sum);
+    }
+}
---
+++ Que1.java
@@ -0,0 +1,11 @@
+package Lecture1;
+
+public class Que1 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+    
+}
