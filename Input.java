cb30c70937c6eb78879903696ddcf1c7dfa83ba8
Karan Lingayat <karan942006@gmail.com>
2026-05-29 20:26:33 +0530

    Files Added

---
+++ Input.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class Input {
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name:");  
+        String name = sc.nextLine();
+        System.out.println("Your Name Is: " + name);
+    }
+}
---
+++ Que2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Que2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Que2: Write a program to add two numbers entered by the user and Print");
+        System.out.println("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        System.out.println("The sum of the two numbers is: " + sum);
+    }
+}
