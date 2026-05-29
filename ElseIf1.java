4c58d8a9924a6cd14d1216914959e4522d363eaf
Karan Lingayat <karan942006@gmail.com>
2026-05-29 20:43:57 +0530

    Update ElseIf1.java

--- ElseIf1.java
+++ ElseIf1.java
@@ -2,6 +2,16 @@ import java.util.*;
 
 public class ElseIf1{
 public static void main(String args[]){
-
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+if(a == b){
+    System.out.println("Equal");
+}
+else (a > b){
+    System.out.println("a is Greater");
+}
+else{
+    System.out.println("b is Greater");
 }
 }
