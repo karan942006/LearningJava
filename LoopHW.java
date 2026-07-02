9c46a7c8837814c33744eef54161a1732047eff9
Karan Lingayat <karan942006@gmail.com>
2026-07-02 17:22:03 +0530

    Refactor LoopHW class and improve input prompt

--- LoopHW.java
+++ LoopHW.java
@@ -1,13 +1,17 @@
 import java.util.*;
 
-public class LoopHW{
-public static void main(String args[]){
+public class LoopHW {
+    public static void main(String[] args) {
 
-Scanner sc = new Scanner(System.in);
-int n = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
 
-for(int i=1; i<=n; i++){
-System.out.println("Hello World");
-}
-}
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Hello World");
+        }
+
+        sc.close();
+    }
 }
