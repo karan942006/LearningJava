38b637f38dd5d2c3db0945c5ca09ad9a6551dff2
Karan Lingayat <karan942006@gmail.com>
2026-07-02 17:20:44 +0530

    Refactor main method and update output messages
    Updated the main method to improve formatting and output message.

--- Que2.java
+++ Que2.java
@@ -1,14 +1,19 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Que2 {
-    public static void main(String args[]){
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Que2: Write a program to add two numbers entered by the user and Print");
-        System.out.println("Enter two numbers: ");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Que2: Write a program to add two numbers entered by the user and print the result.");
+        System.out.print("Enter two numbers: ");
+
         int a = sc.nextInt();
         int b = sc.nextInt();
 
         int sum = a + b;
+
         System.out.println("The sum of the two numbers is: " + sum);
+
+        sc.close();
     }
 }
