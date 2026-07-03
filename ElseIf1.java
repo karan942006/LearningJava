664635529693aa980f5f0f156bef41beb7998a71
Karan Lingayat <karan942006@gmail.com>
2026-07-03 15:18:15 +0530

    Refactor ElseIf1.java for better readability
    Refactored ElseIf1 class to improve formatting and added scanner close.

--- ElseIf1.java
+++ ElseIf1.java
@@ -1,21 +1,22 @@
 import java.util.*;
 
-public class ElseIf1{
-public static void main(String args[]){
-Scanner sc = new Scanner(System.in);
-<<<<<<< HEAD
-=======
-System.out.println("Enter 2 numbers.");
->>>>>>> d61de2915e9b4732e845a24233323cdb8d66d697
-int a = sc.nextInt();
-int b = sc.nextInt();
-if(a == b){
-    System.out.println("Equal");
-}
-else if(a > b){
-    System.out.println("a is Greater");
-}
-else{
-    System.out.println("b is Greater");
-}
+public class ElseIf1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 2 numbers.");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a == b) {
+            System.out.println("Equal");
+        } else if (a > b) {
+            System.out.println("a is Greater");
+        } else {
+            System.out.println("b is Greater");
+        }
+
+        sc.close();
+    }
 }
