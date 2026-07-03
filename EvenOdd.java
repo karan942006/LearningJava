48dd69a64683d6130c1a0be0069289fd81e55598
Karan Lingayat <karan942006@gmail.com>
2026-07-03 15:18:53 +0530

    Improve EvenOdd main method structure
    Refactor main method for better readability and resource management.

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,15 +1,17 @@
 import java.util.*;
 
 public class EvenOdd {
-public static void main(String args[]){
-Scanner sc = new Scanner(System.in);
-int n = sc.nextInt();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
 
-if(n%2 == 0){
-System.out.println("Even");
-}
-else{
-System.out.println("Odd");
-}
-}    
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+        sc.close();
+    }
 }
