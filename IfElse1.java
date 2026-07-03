b955348c1aa558bf5636d35103c80caa0858185f
Karan Lingayat <karan942006@gmail.com>
2026-07-03 15:19:25 +0530

    Refactor IfElse1.java for improved readability

--- IfElse1.java
+++ IfElse1.java
@@ -1,15 +1,17 @@
 import java.util.*;
 
 public class IfElse1 {
-public static void main(String args[]){
-Scanner sc = new Scanner(System.in);
-int age = sc.nextInt();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
 
-if(age>18){
-System.out.println("Adult");
-}
-else{
-System.out.println("Not Adult");
-}
-}    
+        int age = sc.nextInt();
+
+        if (age > 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Not Adult");
+        }
+
+        sc.close();
+    }
 }
